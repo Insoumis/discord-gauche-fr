550c4b6bab50e6aa90456b2c1b542723dfb53033
K13V <baetens.julien@gmail.com>
2026-01-07 00:55:38 +0100

    Présentation de la barricade

--- discord-gauche/src/App.jsx
+++ discord-gauche/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer' // Correction : pas d'accolades
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import Barricade from './assets/Barricade.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +19,30 @@ function App() {
 
 
       <main className="flex-grow container bg-black-100 mx-auto flex flex-col items-center justify-center">
-        <nav>
-
-        </nav>
+      <div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
+        <div class="md:flex flex-col">
+          <div class="md:shrink-0">
+            <img
+              class=" w-full object-cover md:h-full"
+              src={Barricade}
+              alt="Modern building architecture"
+            />
+          </div>
+          <div class="p-8">
+            <div class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">La Barricade</div>
+            <a href="#" class="mt-1 block text-lg leading-tight font-medium text-black hover:underline">
+              La plus grande communauté de gauche sur internet
+            </a>
+            <p class="mt-2 text-gray-500">
+              La Barricade est un serveur discord d'action citoyennes et bénévoles en faveur d'un programme de gauche prônant le progrès social.
+              Ce serveur, initialement pûrement insoumis, s'est engagé pendant les campagnes Présidentielles de Jean Luc Mélenchon en 2017 et 2022 ainsi qu'en faveur de la NUPES et du NFP en 2022 et 2024.
+              
+            </p>
+          </div>
+        </div>
+      </div>
+          
+        
       </main>
 
 
