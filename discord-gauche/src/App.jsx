ad82803dd6206f5764f99409d381d8d98a35a349
erzender <>
2026-02-06 17:09:35 +0100

    added customn shelf component

--- discord-gauche/src/App.jsx
+++ discord-gauche/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Barricade from './assets/Barricade.jpg'
 import './index.css'
+import Shelf from './components/Shelf'
 
 function App() {
   // Juste tes données de base
@@ -61,57 +62,27 @@ function App() {
     ]
   })
 
-  const [selectedBackground, setselectedBackground] = useState(posts.featured[0].banner)
+  const [selectedBackground, setselectedBackground] = useState({ set: "", idx: 0 })
 
   return (
     // 1. Structure fixe : hauteur écran bloquée
     <div className="h-screen flex flex-col bg-noir text-gray-800 overflow-hidden font-[Grotesk]">
-      <div className="absolute w-full h-[700px] bg-cover mask-b-from-10% mask-b-to-80% opacity-30 transition-all duration-500" style={{ backgroundImage: `url(${selectedBackground})` }} />
-
+      {Object.keys(posts).map(set => posts[set].map((post, idx) => (
+        <div
+          className="absolute w-full h-[700px] bg-cover mask-b-from-10% mask-b-to-80% opacity-30 transition-all duration-500"
+          style={{ backgroundImage: `url(${post.banner})`, opacity: selectedBackground.set === set && selectedBackground.idx === idx ? "30%" : "0%" }}
+        />
+      )))}
 
       {/* 2. Zone centrale : prend l'espace restant et scroll si besoin */}
       <main className="flex flex-col py-18 overflow-hidden overflow-y-scroll text-blanc absolute top-0 bottom-0 right-0 left-0">
 
         {/* Liste des posts mis en avant */}
-        <div className="flex flex-row overflow-hidden overflow-x-auto min-h-[400px]">
-          {posts.featured.map((post) => (
-            <div key={post.id} className="ml-6 first:w-[644px] w-[244px] overflow-hidden mb-8 shrink-0 transition-all">
-              <div className="md:flex flex-col h-[400px]">
-                <a onFocus={() => setselectedBackground(post.banner)} onMouseOver={() => setselectedBackground(post.banner)} href="#" className="imageAnchor outline-none p-[2px] border-2 border-transparent focus:border-blanc transition-all duration-500 text-transparent focus:border-blanc focus:transform-[matrix(0.8,0.05,0,0.8,1,0)] focus:text-blanc hover:border-blanc hover:transform-[matrix(0.8,0.05,0,0.8,1,0)] hover:text-blanc">
-                  <img
-                    className="w-full h-[360px] object-cover"
-                    src={post.image}
-                    alt={post.title}
-                  />
-                  <b className="absolute text-2xl font-[Mohave] tracking-wide  uppercase m-2">
-                    {post.title}
-                  </b>
-                </a>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Shelf posts={posts.featured} set="featured" setselectedBackground={setselectedBackground} emphasis />
 
         {/* Liste des jeux */}
         <h1 className="ml-6 mb-2 text-2xl font-extrabold">Les Jeux</h1>
-        <div className="flex flex-row overflow-hidden overflow-x-auto min-h-[400px]">
-          {posts.games.map((post) => (
-            <div key={post.id} className="ml-6 w-[244px] overflow-hidden mb-8 shrink-0 transition-all">
-              <div className="md:flex flex-col h-[400px]">
-                <a onFocus={() => setselectedBackground(post.banner)} onMouseOver={() => setselectedBackground(post.banner)} href="#" className="imageAnchor outline-none p-[2px] border-2 border-transparent focus:border-blanc transition-all duration-500 text-transparent focus:border-blanc focus:transform-[matrix(0.8,0.05,0,0.8,1,0)] focus:text-blanc hover:border-blanc hover:transform-[matrix(0.8,0.05,0,0.8,1,0)] hover:text-blanc">
-                  <img
-                    className="w-full h-[360px] object-cover"
-                    src={post.image}
-                    alt={post.title}
-                  />
-                  <b className="absolute text-2xl font-[Mohave] tracking-wide  uppercase m-2">
-                    {post.title}
-                  </b>
-                </a>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Shelf posts={posts.games} set="games" setselectedBackground={setselectedBackground} />
       </main>
 
       <Header />
