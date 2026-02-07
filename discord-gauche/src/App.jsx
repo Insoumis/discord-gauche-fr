ec50510bab41d4d593df93da9262782abf457a51
erzender <>
2026-02-07 22:22:54 +0100

    added websites shelf

--- discord-gauche/src/App.jsx
+++ discord-gauche/src/App.jsx
@@ -5,7 +5,7 @@ import Post from './components/Post'
 import './index.css'
 import Shelf from './components/Shelf'
 import { getContent, getContentEntry, searchContent } from './utils/content'
-import { FaFilm, FaGamepad, FaHeadphones } from 'react-icons/fa6'
+import { FaFilm, FaGamepad, FaHeadphones, FaInternetExplorer } from 'react-icons/fa6'
 import About from './components/About'
 
 function App() {
@@ -70,9 +70,13 @@ function App() {
           {!!posts.games.length && <h1 className="ml-6 mb-2 text-2xl font-extrabold flex flex-row items-center"><FaGamepad /> Les Jeux</h1>}
           {!!posts.games.length && <Shelf posts={posts.games} set="games" setSelectedBackground={setSelectedBackground} setSelectedPost={setSelectedPost} />}
 
+          {/* Liste des sites */}
+          {!!posts.websites.length && <h1 className="ml-6 mb-2 text-2xl font-extrabold flex flex-row items-center"><FaInternetExplorer /> Les Sites/Outils Web</h1>}
+          {!!posts.websites.length && <Shelf posts={posts.websites} set="websites" setSelectedBackground={setSelectedBackground} setSelectedPost={setSelectedPost} />}
+
           {/* Liste des vidéos */}
           {!!posts.music.length && <h1 className="ml-6 mb-2 text-2xl font-extrabold flex flex-row items-center"><FaHeadphones /> Les Sons</h1>}
-          {!!posts.music.length && <Shelf posts={posts.music} set="videos" setSelectedBackground={setSelectedBackground} setSelectedPost={setSelectedPost} />}
+          {!!posts.music.length && <Shelf posts={posts.music} set="music" setSelectedBackground={setSelectedBackground} setSelectedPost={setSelectedPost} />}
 
           {/* Liste des sons */}
           {!!posts.videos.length && <h1 className="ml-6 mb-2 text-2xl font-extrabold flex flex-row items-center"><FaFilm /> Les Montages cringe</h1>}
